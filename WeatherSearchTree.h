a7ea5e1e77413a947dc0ff768a4dbeeceb8008e2
Maya-Romer <mayarom@pdx.edu>
2024-12-01 18:37:19 -0800

    doxygen style comments

--- WeatherSearchTree.h
+++ WeatherSearchTree.h
@@ -1,23 +1,30 @@
 #pragma once
 
-//@description: a struct to hold weather measurement data
+/** a struct to hold weather measurement data */
 struct weather_data {
-    //@member timestamp: the time this reading was taken. Hour, minute, and second are all 0.
+    /** The time this reading was taken. Hour, minute, and second are all 0. */
     time_t timestamp;
-    //@member temperature: the measured temperature, in degrees Celsius
+    /** The measured temperature, in degrees Celsius */
     float temperature;
-    //@member humidity: the measured humidity, in percent
+    /** The measured humidity, in percent */
     float humidity;
 };
 
-//@description: inserts a new weather data element into the tree
-//@param data: the data to insert
+/**
+* Inserts a new weather data element into the tree
+* @param data: the data to insert
+*/
 void insertWeatherDataIntoTree(struct weather_data* data);
-//@description: searches tree for data matching a specified timestamp
-//@param timestamp: the timestamp to search for. Hour, minute, and second should all be 0.
-//@return: a pointer to the weather_data with a matching timestamp. NULL if no match found, unpredictable if multiple matches.
+
+/*
+* Searches tree for data matching a specified timestamp.
+* @param timestamp: the timestamp to search for. Hour, minute, and second should all be 0.
+* @return: a pointer to the weather_data with a matching timestamp. NULL if no match found, unpredictable if multiple matches.
+*/
 struct weather_data* searchTree(time_t timestamp);
-//@description: prints all weather_data in order of timestamp.
+
+/** Prints all weather_data in order of timestamp. */
 void printTreeInOrder();
-//@description: deletes a tree and frees all used memory.
+
+/** Deletes a tree and frees all used memory. */
 void deleteTree();
